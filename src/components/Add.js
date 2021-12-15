1f19afbfe46a347c4787bce210325fbe1c891a8d
Michal <mincoslav@gmail.com>
2021-12-15 02:05:36 +0100

    fixed the reviews

--- src/components/Add.js
+++ src/components/Add.js
@@ -6,119 +6,119 @@ import "./TrendingMovies/Trending.css";
 import { PagePagination } from "./TrendingMovies/PagePagination";
 
 export const Add = () => {
-  const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
-  const [content, setContent] = useState([]);
-  const [recomended, setRecomended] = useState([]);
-  const [page, setPage] = useState(1);
+	const [query, setQuery] = useState("");
+	const [results, setResults] = useState([]);
+	const [content, setContent] = useState([]);
+	const [recomended, setRecomended] = useState([]);
+	const [page, setPage] = useState(1);
 
-  const onChange = (e) => {
-    e.preventDefault();
+	const onChange = (e) => {
+		e.preventDefault();
 
-    setQuery(e.target.value);
+		setQuery(e.target.value);
 
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.errors) {
-          // checking for errors
-          setResults(data.results);
-        } else {
-          setResults([]); // no results
-        }
-      });
-  };
+		fetch(
+			`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				if (!data.errors) {
+					// checking for errors
+					setResults(data.results);
+				} else {
+					setResults([]); // no results
+				}
+			});
+	};
 
-  // trending movies on the main page
-  const fetchTrendingMovies = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_TMDB_KEY}&page=${page}`
-    );
-    // console.log(data);
-    
-    // for (let index = 0; index < data.results.length; index++) {
-    //   let external_id = await axios.get(`https://api.themoviedb.org/3/movie/${data.results[index].id}/external_ids?api_key=${process.env.REACT_APP_TMDB_KEY}`)
-    //   //console.log(external_id.data.imdb_id)
-    //   external_id.data.imdb_id = external_id.data.imdb_id.slice(2, external_id.data.imdb_id.length).replace(/^0+/, '')
-      
-    //   // data.results[index].id = external_id['imdb_id']
-    //   console.log(external_id.data.imdb_id)
-    // }
+	// trending movies on the main page
+	const fetchTrendingMovies = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_TMDB_KEY}&page=${page}`
+		);
+		// console.log(data);
 
-    setContent(data.results);
-  };
-  // Recomended movies
-  const fetchRecomendedMovies = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/recomended/movie/day?api_key=${process.env.REACT_APP_TMDB_KEY}&page=${page}`
-    );
-    // console.log(data);
+		// for (let index = 0; index < data.results.length; index++) {
+		//   let external_id = await axios.get(`https://api.themoviedb.org/3/movie/${data.results[index].id}/external_ids?api_key=${process.env.REACT_APP_TMDB_KEY}`)
+		//   //console.log(external_id.data.imdb_id)
+		//   external_id.data.imdb_id = external_id.data.imdb_id.slice(2, external_id.data.imdb_id.length).replace(/^0+/, '')
 
-    setRecomended(data.results);
-  };
+		//   // data.results[index].id = external_id['imdb_id']
+		//   console.log(external_id.data.imdb_id)
+		// }
+		console.log(data.results);
+		setContent(data.results);
+	};
+	// Recomended movies
+	const fetchRecomendedMovies = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/recomended/movie/day?api_key=${process.env.REACT_APP_TMDB_KEY}&page=${page}`
+		);
+		// console.log(data);
 
-  useEffect(() => {
-    fetchTrendingMovies();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page.data]);
+		setRecomended(data.results);
+	};
 
-  return (
-    <div className="add-page">
-      <div className="container">
-        <div className="add-content">
-          <div className="input-wrapper">
-            <input
-              type="text"
-              placeholder="Search for a movie"
-              value={query}
-              onChange={onChange}
-            />
-          </div>
+	useEffect(() => {
+		fetchTrendingMovies();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [page.data]);
 
-          {results.length > 0 && (
-            <ul className="results">
-              {results.map((movie) => (
-                <li key={movie.id}>
-                  <ResultCard movie={movie} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
-      <h2>Recomended Movies</h2>
-      <div className="trending">
-        {recomended &&
-          recomended.map((c) => (
-            <MovieContent
-              key={c.id}
-              id={c.id}
-              poster={c.poster_path}
-              title={c.title || c.name}
-              date={c.first_air_date || c.release_date}
-              media_type={c.media_type}
-              vote_average={c.vote_average}
-            />
-          ))}
-      </div>
-      <h2>Trending Movies</h2>
-      <div className="trending">
-        {content &&
-          content.map((c) => (
-            <MovieContent
-              key={c.id}
-              id={c.id}
-              poster={c.poster_path}
-              title={c.title || c.name}
-              date={c.first_air_date || c.release_date}
-              media_type={c.media_type}
-              vote_average={c.vote_average}
-            />
-          ))}
-      </div>
-      <PagePagination setPage={setPage} />
-    </div>
-  );
+	return (
+		<div className="add-page">
+			<div className="container">
+				<div className="add-content">
+					<div className="input-wrapper">
+						<input
+							type="text"
+							placeholder="Search for a movie"
+							value={query}
+							onChange={onChange}
+						/>
+					</div>
+
+					{results.length > 0 && (
+						<ul className="results">
+							{results.map((movie) => (
+								<li key={movie.id}>
+									<ResultCard movie={movie} />
+								</li>
+							))}
+						</ul>
+					)}
+				</div>
+			</div>
+			<h2>Recomended Movies</h2>
+			<div className="trending">
+				{recomended &&
+					recomended.map((c) => (
+						<MovieContent
+							key={c.id}
+							id={c.id}
+							poster={c.poster_path}
+							title={c.title || c.name}
+							date={c.first_air_date || c.release_date}
+							media_type={c.media_type}
+							vote_average={c.vote_average}
+						/>
+					))}
+			</div>
+			<h2>Trending Movies</h2>
+			<div className="trending">
+				{content &&
+					content.map((c) => (
+						<MovieContent
+							key={c.id}
+							id={c.id}
+							poster={c.poster_path}
+							title={c.title || c.name}
+							date={c.first_air_date || c.release_date}
+							media_type={c.media_type}
+							vote_average={c.vote_average}
+						/>
+					))}
+			</div>
+			<PagePagination setPage={setPage} />
+		</div>
+	);
 };
--- src/components/MovieModal/MovieModal.js
+++ src/components/MovieModal/MovieModal.js
@@ -1,307 +1,298 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
+
 import { makeStyles } from "@material-ui/core";
 import {
-  IMAGE_500,
-  IMAGE_300,
-  UNAVAILABLELandscape,
-  UNAVAILABLE,
+	IMAGE_500,
+	IMAGE_300,
+	UNAVAILABLELandscape,
+	UNAVAILABLE
 } from "../../configs";
 import axios from "axios";
-import { video, useVideo } from "react";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import "./mm.css";
-import { useParams } from "react-router-dom";
 import CarouselActors from "./CarouselActors";
 import "./CarouselActors.css";
 import { Chip } from "@material-ui/core";
 import Rating from "./Rating";
 import { useMsal, useIsAuthenticated } from "@azure/msal-react";
-import { Comments } from "../Comment/SingleComment";
+import HoverRating from "./Rating";
 
 const useStyles = makeStyles((Theme) => ({
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 0,
-    transparent: true,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    backdrop_path: true,
-    scroll: true,
-  },
-  paper: {
-    width: "70%",
-    height: "80%",
-    backgroundColor: "#e99696",
-    border: "1px solid #e99696",
-    borderRadius: 5,
-    color: "white",
-    boxShadow: 24,
-    p: 4,
-    padding: Theme.spacing(1, 1, 3),
-  },
+	modal: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		margin: 0,
+		transparent: true,
+		backgroundColor: "rgba(0, 0, 0, 0.5)",
+		backdrop_path: true,
+		scroll: true
+	},
+	paper: {
+		width: "70%",
+		height: "80%",
+		backgroundColor: "#e99696",
+		border: "1px solid #e99696",
+		borderRadius: 5,
+		color: "white",
+		boxShadow: 24,
+		p: 4,
+		padding: Theme.spacing(1, 1, 3)
+	}
 }));
 
 export default function TransitionsModal({
-  children,
-  id,
-  media_type,
-  props,
-  i,
-  genres,
-  crew,
-  member,
+	children,
+	id,
+	media_type,
+	props,
+	i,
+	genres,
+	crew,
+	member
 }) {
-  const classes = useStyles();
-  const [open, setOpen] = useState(false);
-  const [content, setContent] = useState();
-  const [video, setVideo] = useState();
-  const [movieInfo, setMovieInfo] = useState();
-  const [cast, setCast] = useState();
+	const classes = useStyles();
+	const [open, setOpen] = useState(false);
+	const [content, setContent] = useState();
+	const [video, setVideo] = useState();
+	const [movieInfo, setMovieInfo] = useState();
+	const [cast, setCast] = useState();
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleOpen = () => {
+		setOpen(true);
+	};
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  const fetchData = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
-    );
-    //console.log(data);
-    setContent(data);
-  };
+	const fetchData = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+		);
+		//console.log(data);
+		setContent(data);
+	};
 
-  const fetchVideo = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
-    );
+	const fetchVideo = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+		);
 
-    setVideo(data.results[0]?.key);
-  };
+		setVideo(data.results[0]?.key);
+	};
 
-  // movie credits
-  const fetchCredits = async () => {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    );
+	// movie credits
+	const fetchCredits = async () => {
+		const response = await axios.get(
+			`https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${process.env.REACT_APP_TMDB_KEY}`
+		);
 
-    //const cast = await response.json();
-    // setCast(response);
+		const director = response.data.crew.find(
+			(director) => director.job === "Director"
+		);
 
-    const director = response.data.crew.find(
-      (director) => director.job === "Director"
-    );
+		setCast(director.name);
+	};
 
-    //console.log(director);
+	//   // searching the name of the director
 
-    setCast(director.name);
-  };
+	//   console.log(response.data.crew.director);
 
-  //   // searching the name of the director
+	//   const credits = response.data;
+	//   return { director: director, credits: credits };
 
-  //   console.log(response.data.crew.director);
+	// };
 
-  //   const credits = response.data;
-  //   return { director: director, credits: credits };
+	// const directors = cast.filter((member) => member.job === 'Directing');
 
-  // };
+	// useEffect(() => {
+	//   axios.get(
+	//     `https://api.themoviedb.org/3/${selectedMovie}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`,
+	//   ).then((response) => setMovieInfo(response.data));
+	// }, [selectedMovie]);
 
-  // const directors = cast.filter((member) => member.job === 'Directing');
+	useEffect(() => {
+		fetchData();
+		fetchVideo();
+		fetchCredits();
+	}, []);
 
-  // useEffect(() => {
-  //   axios.get(
-  //     `https://api.themoviedb.org/3/${selectedMovie}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`,
-  //   ).then((response) => setMovieInfo(response.data));
-  // }, [selectedMovie]);
+	const authenticated = useIsAuthenticated();
+	const { accounts } = useMsal();
+	const name = accounts[0] && accounts[0].name;
 
-  useEffect(() => {
-    fetchData();
-    fetchVideo();
-    fetchCredits();
-  }, []);
+	return (
+		<>
+			<div
+				className="media"
+				onClick={handleOpen}
+				style={{ cursor: "pointer" }}
+				color="inherit"
+			>
+				{children}
+			</div>
+			<Modal
+				className={classes.modal}
+				aria-labelledby="transition-modal-title"
+				aria-describedby="transition-modal-description"
+				open={open}
+				onClose={handleClose}
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+					timeout: 500
+				}}
+				style={{ overflow: "scroll" }}
+			>
+				<Fade in={open}>
+					{content && (
+						<div className={classes.paper}>
+							<div className="MovieModal">
+								<img
+									className="MovieModal_portrait"
+									alt={content.name || content.title}
+									src={
+										content.poster_path
+											? `${IMAGE_300}/${content.poster_path}`
+											: UNAVAILABLE
+									}
+									alt={content.name || content.title}
+									className="MovieModal_portrait"
+								/>
+								<img
+									src={
+										content.backdrop_path
+											? `${IMAGE_500}/${content.backdrop_path}`
+											: UNAVAILABLELandscape
+									}
+									alt={content.name || content.title}
+									className="MovieModal_landscape"
+								/>
 
-  const authenticated = useIsAuthenticated();
-  const { accounts } = useMsal();
-  const name = accounts[0] && accounts[0].name;
+								<div className="MovieModal_about">
+									<span className="MovieModal_title">
+										{content.name || content.title} (
+										{(
+											content.first_air_date ||
+											content.release_date ||
+											"-"
+										).substring(0, 4)}
+										)
+									</span>
 
-  return (
-    <>
-      <div
-        className="media"
-        onClick={handleOpen}
-        style={{ cursor: "pointer" }}
-        color="inherit"
-      >
-        {children}
-      </div>
-      <Modal
-        className={classes.modal}
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-        style={{ overflow: "scroll" }}
-      >
-        <Fade in={open}>
-          {content && (
-            <div className={classes.paper}>
-              <div className="MovieModal">
-                <img
-                  className="MovieModal_portrait"
-                  alt={content.name || content.title}
-                  src={
-                    content.poster_path
-                      ? `${IMAGE_300}/${content.poster_path}`
-                      : UNAVAILABLE
-                  }
-                  alt={content.name || content.title}
-                  className="MovieModal_portrait"
-                />
-                <img
-                  src={
-                    content.backdrop_path
-                      ? `${IMAGE_500}/${content.backdrop_path}`
-                      : UNAVAILABLELandscape
-                  }
-                  alt={content.name || content.title}
-                  className="MovieModal_landscape"
-                />
+									{content.tagline && (
+										<i className="tagline">{content.tagline}</i>
+									)}
 
-                <div className="MovieModal_about">
-                  <span className="MovieModal_title">
-                    {content.name || content.title} (
-                    {(
-                      content.first_air_date ||
-                      content.release_date ||
-                      "-"
-                    ).substring(0, 4)}
-                    )
-                  </span>
+									<div>
+										<span>
+											{content.genres &&
+												content.genres.slice(0, 5).map((genre, i) => (
+													<Chip
+														color="secondary"
+														style={{ margin: 2 }}
+														label={genre.name}
+														key={i}
+														text="bold"
+														className="genres__item"
+													>
+														{genre.name}
+													</Chip>
+												))}
+										</span>
+									</div>
 
-                  {content.tagline && (
-                    <i className="tagline">{content.tagline}</i>
-                  )}
+									<div className="MovieModal_movieInf">
+										<div>
+											<span>
+												Release Date: <i>{content?.release_date}</i>
+											</span>
+										</div>
 
-                  <div>
-                    <span>
-                      {content.genres &&
-                        content.genres.slice(0, 5).map((genre, i) => (
-                          <Chip
-                            color="secondary"
-                            style={{ margin: 2 }}
-                            label={genre.name}
-                            key={i}
-                            text="bold"
-                            className="genres__item"
-                          >
-                            {genre.name}
-                          </Chip>
-                        ))}
-                    </span>
-                  </div>
+										<div>
+											<span>
+												IMDB Rating: <i>{content?.vote_average}</i>
+											</span>
+										</div>
 
-                  <div className="MovieModal_movieInf">
-                    <div>
-                      <span>
-                        Release Date: <i>{content?.release_date}</i>
-                      </span>
-                    </div>
+										<div>
+											<span>
+												Language: <i>{content?.original_language}</i>
+											</span>
+										</div>
 
-                    <div>
-                      <span>
-                        IMDB Rating: <i>{content?.vote_average}</i>
-                      </span>
-                    </div>
+										<div>
+											<span>
+												Runtime: <i>{content?.runtime}</i>
+												{" minutes"}
+											</span>
+										</div>
 
-                    <div>
-                      <span>
-                        Language: <i>{content?.original_language}</i>
-                      </span>
-                    </div>
+										<div>
+											<span>
+												Budget: <i>{content?.budget}</i>{" "}
+											</span>
+										</div>
 
-                    <div>
-                      <span>
-                        Runtime: <i>{content?.runtime}</i>
-                        {" minutes"}
-                      </span>
-                    </div>
+										<div>
+											<span>
+												Director: <i>{cast}</i>
+											</span>
+										</div>
+									</div>
+									<span className="MovieModal_description">
+										{content.overview}
+									</span>
 
-                    <div>
-                      <span>
-                        Budget: <i>{content?.budget}</i>{" "}
-                      </span>
-                    </div>
+									<div className="carousel_actors">
+										<CarouselActors media_type={media_type} id={id} />
+									</div>
 
-                    <div>
-                      <span>
-                        Director: <i>{cast}</i>
-                      </span>
-                    </div>
-                  </div>
-                  <span className="MovieModal_description">
-                    {content.overview}
-                  </span>
+									<Button
+										variant="contained"
+										startIcon={<YouTubeIcon />}
+										color="secondary"
+										target="__blank"
+										href={`https://www.youtube.com/watch?v=${video}`}
+									>
+										Watch Trailer
+									</Button>
 
-                  <div className="carousel_actors">
-                    <CarouselActors media_type={media_type} id={id} />
-                  </div>
+									<div className="MovieModal_user_comments">
+										{/* <div className="MovieModal_review_title">
+											<h2>User reviews</h2>
+											<div className="all_user_comments">
+												<div className="single_user_comments">
+													<div className="username_with_stars">
+														<div className="username">
+															<h5>Smilte</h5>
+														</div>
+														<div className="stars"></div>
+													</div>
 
-                  <Button
-                    variant="contained"
-                    startIcon={<YouTubeIcon />}
-                    color="secondary"
-                    target="__blank"
-                    href={`https://www.youtube.com/watch?v=${video}`}
-                  >
-                    Watch Trailer
-                  </Button>
-
-                  <div className="MovieModal_user_comments">
-                    {/* <div className="MovieModal_review_title">
-                      <h2>User reviews</h2>
-                      <div className="all_user_comments">
-                        <div className="single_user_comments">
-                          <div className="username_with_stars">
-                            <div className="username">
-                              <h5>Smilte</h5>
-                            </div>
-                            <div className="stars"></div>
-                          </div>
-
-                          <div className="comment">
-                            "hkzjshdkjshdk"
-                          </div>
-                        </div>
-                      </div>
-                    </div> */}
-                    <div className="MovieModal_write_review_title">
-                      <div>
-                        <h4>Write a review</h4>
-                        <Rating />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
-        </Fade>
-      </Modal>
-    </>
-  );
+													<div className="comment">
+														"hkzjshdkjshdk"
+													</div>
+												</div>
+											</div>
+										</div> */}
+										<div className="MovieModal_write_review_title">
+											<div>
+												<h4>Write a review</h4>
+												<HoverRating id={id}/>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					)}
+				</Fade>
+			</Modal>
+		</>
+	);
 }
