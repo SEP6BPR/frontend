93630ba295a1266f7d8ac41a60857df7a785d791
Marwan Summakieh <42711091+MarwanSummakieh@users.noreply.github.com>
2021-12-15 20:22:46 +0100

    supoosed to create option from list
    created a function that takes the vlaues from the response and creates options corrosponding to them but for somereason it returns undefined

--- src/components/ListOfLists.js
+++ src/components/ListOfLists.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useMsal } from "@azure/msal-react";
 import axios from "axios";
 import { ButtonGroup } from "@material-ui/core";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, FormText } from "react-bootstrap";
 import DropdownItem from "react-bootstrap/esm/DropdownItem";
 import { Button } from "react-bootstrap";
 import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
@@ -11,118 +11,47 @@ import "./ListOfList.css";
 import Select from "react-select";
 
 export const ListOfLists = () => {
-  var data = [];
-  var lists = [];
-  const { accounts } = useMsal();
-  const username = accounts[0] && accounts[0].username;
-  const [list, setList] = useState(data);
-  //   let listArr = list.split(',');
 
-  const fetchData = () => {
-    axios
-      .get(`https://not-pirate-bay.azurewebsites.net/user/${username}/lists`)
-      .then((response) => {
-        setList(response.data.list);
+    const { accounts } = useMsal();
+    const username = accounts[0] && accounts[0].username;
+    const [list, setList] = useState([]);
+    let listNames = [];
+    //   let listArr = list.split(',');
+
+    const fetchData = () => {
+        axios
+            .get(`https://not-pirate-bay.azurewebsites.net/user/${username}/lists`)
+            .then((response) => {
+                const res = response.data.list_ids
+                 setList(res);                
+                
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        console.log(creatListMenu())
+    };
+    const creatListMenu = () => {
+        for (let i = 0; i < list.length; i++) {
+            listNames.push(list[i].list_name);
+            return <option key={list[i].list_id}>{listNames[i]}</option>    
+        }
         
-        console.log(list);
-        // console.log(list.list_name);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+    }
+        useEffect(() => {
+            fetchData();
+        });
 
- 
-
-  // const demo = {
-  //   [
-  //     {
-  //       "id": 1,
-  //       "name": "demo",
-  //     },
-  //     {
-  //       "id": 2,
-  //       "name": "poop",
-  //     },
-  //   ]
-  // }
-
-  // useEffect(() => {
-  //   fetchData();
-  // });
-
-  return (
-    // <div>
-    //   <span>
-    //     <button onClick={fetchData}>update</button>
-    //   </span>
-
-    //   <select>
-    //     {list.list_name && list.map((list_ids, obj) => (
-    //         <option key={obj} label={list_ids.name} >
-    //           {list.list_name}
-    //         </option>
-    //       ))}
-    //   </select>
-    // </div>
-
-    <div>
-      {/* <Select onClick={fetchData} className="select_hover"
-      onChange={fetchData}>
-        <option disabled selected>
-          Select your list
-        </option> */}
-      {/* <button onClick={fetchData}>click</button>
-      <div>
-        
-        list:
-        {list for.map(lists => { return(
-            <div key={lists} value={list.data}>
-              {lists.name}
+        return (
+            <div>
+                <span>
+                    <button onClick={fetchData}>update</button>
+                </span>
+        <select>
+           {creatListMenu}
+        </select>
             </div>
-          )})}
-      </div> */}
-      {/* <div>
-        {data &&
-          data.map((lists) => {
-            return <p>{list}</p>;
-          })}
-          </div> */}
-      {/* </Select> */}
-    </div>
+        );
+    };
 
-    // <div className="drop-down">
-    //   <span>
-    //     <button onClick={fetchData}>update lists</button>
-    //   </span>
-    //   <select>
-    //     {data.map((obj) => {
-    //       return <option value={obj.list_ids}>{obj.list_name}</option>;
-    //     })}
-    //   </select>
-    // </div>
-
-    //     <Dropdown> {list &&}
-    //   <Dropdown.Toggle variant="success" id="dropdown-basic" onClick={fetchData}>
-    //     Dropdown Button
-    //   </Dropdown.Toggle>
-
-    //   <Dropdown.Menu> { list.list_name && (
-    //       <Dropdown.Item value={data.list_ids}>{list.list_name}</Dropdown.Item>
-    //   )}
-    //     {/* <Dropdown.Item href="#/action-1">{data.list_name}</Dropdown.Item> */}
-
-    //   </Dropdown.Menu>
-
-    // </Dropdown>
-
-    // { list && (
-    // <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-    //   <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    //   <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    //   <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-    // </DropdownButton>
-    // )}
-  );
-};
 export default ListOfLists;
